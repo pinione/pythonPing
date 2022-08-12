e05dacbfcf91b7fcbc852ae1504b725ab4a5e621
Bart <bart.git@p.com7.pl>
2022-08-12 18:04:20 +0200

    refactoring variable

--- pythonPing.py
+++ pythonPing.py
@@ -8,17 +8,17 @@ from datetime import datetime
 now = datetime.now()
 dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 
-stan = None
+state_of_system = None
 
 if len(sys.argv)<2:
     print("You need to add IP as argument")
 
 while True:
     w = ping(sys.argv[1], count=2, interval=1, timeout=2, id=None, source=None, family=None, privileged=False)
-    if stan == w.is_alive:
+    if state_of_system == w.is_alive:
         pass
     else:
-        stan = w.is_alive
+        state_of_system = w.is_alive
         if w.is_alive:
             print(f'Host {sys.argv[1]} is online: {dt_string}')
         else:
